e636054943def2c65a172f6bda81e5d9e5bdafdf
Jose1060 <84330500+Jose1060@users.noreply.github.com>
2021-11-22 09:41:38 -0500

    A1_PrincipalMenu

--- app/src/main/java/com/aplication/techforest/MainActivity.kt
+++ app/src/main/java/com/aplication/techforest/MainActivity.kt
@@ -3,36 +3,25 @@ package com.aplication.techforest
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.ExperimentalFoundationApi
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
+import com.aplication.techforest.ui.HomeScreen
 import com.aplication.techforest.ui.theme.TechForestTheme
 
 class MainActivity : ComponentActivity() {
+    @ExperimentalFoundationApi
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             TechForestTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(color = MaterialTheme.colors.background) {
-                    Greeting("Android")
-                }
+                HomeScreen()
             }
         }
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    TechForestTheme {
-        Greeting("Android")
-    }
-}
